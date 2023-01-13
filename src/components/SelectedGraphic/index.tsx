96484eb367899da36c87963a27b5871296ade7bc
Kellbber <kellbberbarcarolo@gmail.com>
2023-01-13 19:00:21 -0300

    fix(format): new prettier aplicated

--- src/components/SelectedGraphic/index.tsx
+++ src/components/SelectedGraphic/index.tsx
@@ -227,219 +227,219 @@ const SelectedGraphic = ({ csv }: Form) => {
     });
   });
   //
-    //filtra os abertos por mes
-    allOpen.find((items: string[]) => {
-      items.find((item: string) => {
-        if (search == "janeiro") {
-          if (item.includes("-01")) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+  //filtra os abertos por mes
+  allOpen.find((items: string[]) => {
+    items.find((item: string) => {
+      if (search == "janeiro") {
+        if (item.includes("-01")) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "fevereiro") {
-          if (item.includes("-01") || item.includes("-02")) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "fevereiro") {
+        if (item.includes("-01") || item.includes("-02")) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "marco") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "marco") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "abril") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "abril") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "maio") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04") ||
-            item.includes("-05")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "maio") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04") ||
+          item.includes("-05")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "junho") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04") ||
-            item.includes("-06")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "junho") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04") ||
+          item.includes("-06")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "julho") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04") ||
-            item.includes("-06") ||
-            item.includes("-07")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "julho") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04") ||
+          item.includes("-06") ||
+          item.includes("-07")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "agosto") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04") ||
-            item.includes("-06") ||
-            item.includes("-07") ||
-            item.includes("-08")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "agosto") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04") ||
+          item.includes("-06") ||
+          item.includes("-07") ||
+          item.includes("-08")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "setembro") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04") ||
-            item.includes("-06") ||
-            item.includes("-07") ||
-            item.includes("-08") ||
-            item.includes("-09")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "setembro") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04") ||
+          item.includes("-06") ||
+          item.includes("-07") ||
+          item.includes("-08") ||
+          item.includes("-09")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "outubro") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04") ||
-            item.includes("-06") ||
-            item.includes("-07") ||
-            item.includes("-08") ||
-            item.includes("-09") ||
-            item.includes("-10")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "outubro") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04") ||
+          item.includes("-06") ||
+          item.includes("-07") ||
+          item.includes("-08") ||
+          item.includes("-09") ||
+          item.includes("-10")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "novembro") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04") ||
-            item.includes("-06") ||
-            item.includes("-07") ||
-            item.includes("-08") ||
-            item.includes("-09") ||
-            item.includes("-10") ||
-            item.includes("-11")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "novembro") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04") ||
+          item.includes("-06") ||
+          item.includes("-07") ||
+          item.includes("-08") ||
+          item.includes("-09") ||
+          item.includes("-10") ||
+          item.includes("-11")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-        if (search == "dezembro") {
-          if (
-            item.includes("-01") ||
-            item.includes("-02") ||
-            item.includes("-03") ||
-            item.includes("-04") ||
-            item.includes("-06") ||
-            item.includes("-07") ||
-            item.includes("-08") ||
-            item.includes("-09") ||
-            item.includes("-10") ||
-            item.includes("-11") ||
-            item.includes("-12")
-          ) {
-            items.filter((value: string) => {
-              if (value.includes(".")) {
-                filteredOpen.push(parseInt(value));
-              }
-            });
-          }
+      }
+      if (search == "dezembro") {
+        if (
+          item.includes("-01") ||
+          item.includes("-02") ||
+          item.includes("-03") ||
+          item.includes("-04") ||
+          item.includes("-06") ||
+          item.includes("-07") ||
+          item.includes("-08") ||
+          item.includes("-09") ||
+          item.includes("-10") ||
+          item.includes("-11") ||
+          item.includes("-12")
+        ) {
+          items.filter((value: string) => {
+            if (value.includes(".")) {
+              filteredOpen.push(parseInt(value));
+            }
+          });
         }
-      });
+      }
     });
-    //
-let payedValue = 0;
-    for(let i = 0; i< filteredPay.length;i++){
-      payedValue += filteredPay[i]
-    }
-    const payed = payedValue;
+  });
+  //
+  let payedValue = 0;
+  for (let i = 0; i < filteredPay.length; i++) {
+    payedValue += filteredPay[i];
+  }
+  const payed = payedValue;
 
-    let openValue = 0;
-    for(let i = 0; i< filteredOpen.length;i++){
-      openValue += filteredOpen[i]
-    }
-    const open = openValue;
+  let openValue = 0;
+  for (let i = 0; i < filteredOpen.length; i++) {
+    openValue += filteredOpen[i];
+  }
+  const open = openValue;
 
-    const inadimplence = open / (open + payed);
+  const inadimplence = open / (open + payed);
 
-    const data = [
-      ["Element", "Valor", { role: "style" }],
-      ["Inadimplência do mês", inadimplence, "rgb(51, 102, 204)"],
-    ];
-    const options = {
-      title: "Valor total",
-    };
+  const data = [
+    ["Element", "Valor", { role: "style" }],
+    ["Inadimplência do mês", inadimplence, "rgb(51, 102, 204)"],
+  ];
+  const options = {
+    title: "Valor total",
+  };
   return (
     <>
       <section className={styles.container}>
@@ -465,17 +465,16 @@ let payedValue = 0;
           <option value="novembro">novembro</option>
           <option value="dezembro">dezembro</option>
         </select>
-       <div className={styles.content}>
-       <Chart
+        <div className={styles.content}>
+          <Chart
             chartType="ColumnChart"
             data={data}
             options={options}
             width="100%"
             height="350px"
           />
-       </div>
+        </div>
       </section>
-    
     </>
   );
 };
